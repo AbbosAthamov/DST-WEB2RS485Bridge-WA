f8cdc7c2752ffa74d66c1b2f4ac1c3eeb55b4a8b
Abbos <67047433+AbbosAthamov@users.noreply.github.com>
2025-08-04 16:41:41 +0500

    Update power.entity.ts

--- src/modules/power/power.entity.ts
+++ src/modules/power/power.entity.ts
@@ -5,22 +5,22 @@ export class Power {
   @PrimaryGeneratedColumn()
   id: number
 
-  @Column({ type: 'int', nullable: false })
+  @Column({ type: 'int', nullable: false, default: 0 })
   out1: number
 
-  @Column({ type: 'int', nullable: false })
+  @Column({ type: 'int', nullable: false, default: 0 })
   out2: number
 
-  @Column({ type: 'int', nullable: false })
+  @Column({ type: 'int', nullable: false, default: 0 })
   out3: number
 
-  @Column({ type: 'int', nullable: false })
+  @Column({ type: 'int', nullable: false, default: 0 })
   out4: number
 
-  @Column({ type: 'int', nullable: false })
+  @Column({ type: 'int', nullable: false, default: 0 })
   out5: number
 
-  @Column({ type: 'int', nullable: false })
+  @Column({ type: 'int', nullable: false, default: 0 })
   out6: number
 
   @Column()
