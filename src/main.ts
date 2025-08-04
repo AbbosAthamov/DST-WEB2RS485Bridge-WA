b053fbebdcb17662b6b76a7fcde20018ca542aa3
Abbos <67047433+AbbosAthamov@users.noreply.github.com>
2025-08-04 20:11:20 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
-  app.enableCors({
-    origin: '*', // Разрешить всем (можно указать конкретный домен)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-
-  await app.listen(4480) // Убедись, что порт соответствует тому, на котором ты запускаешь сервер
+  await app.listen(3000)
 }
 
 bootstrap()
