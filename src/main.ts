f901be9402125d98091d4d9effb31c530ebc4781
Abbos <67047433+AbbosAthamov@users.noreply.github.com>
2025-08-04 20:26:57 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(4480, '0.0.0.0')
+  await app.listen(3000)
 }
 
 bootstrap()
