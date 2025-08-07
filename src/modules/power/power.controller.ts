83b36699889609f3e0245490dd0063ea753c4286
Abbos <67047433+AbbosAthamov@users.noreply.github.com>
2025-08-07 12:43:43 +0500

    InputPorts

--- src/modules/power/power.controller.ts
+++ src/modules/power/power.controller.ts
@@ -7,40 +7,16 @@ import * as moment from 'moment'
 export class PowerController {
   constructor(private readonly powerService: PowerService) {}
 
-  @Get('/always-long')
-  async getStatusAlwaysLongGet(@Req() req): Promise<object> {
-    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.query)
-    return this.powerService.getStatusAlwaysLong()
-  }
-
-  @Post('/always-long')
-  async getStatusAlwaysLongPost(@Req() req): Promise<object> {
-    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.body)
-    return this.powerService.getStatusAlwaysLong()
-  }
-
   @Get()
-  async getStatusGet(@Req() req): Promise<object> {
+  async getStatus(@Req() req): Promise<object> {
     console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.query)
     return this.powerService.getStatus()
   }
 
-  @Get('/getdb')
-  async getAllFromDb(@Req() req): Promise<object> {
-    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip)
-    const data = await this.powerService.getAll()
-    return { count: data.length, data }
-  }
-
-  @Get('ports')
-  async getLatestPorts() {
-    return this.powerService.getLatestPorts();
-  }
-
-
   @Post()
-  async getStatusPost(@Req() req): Promise<object> {
-    console.log(req.body)
+  async postStatus(@Req() req): Promise<object> {
+    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.body)
+
     const data: Partial<Power> = {
       ip: req.ip,
     }
@@ -57,5 +33,52 @@ export class PowerController {
 
     await this.powerService.saveOutpusts(data)
     return this.powerService.getStatus()
+  }
+
+  @Get('/always-long')
+  async getStatusAlwaysLong(@Req() req): Promise<object> {
+    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.query)
+    return this.powerService.getStatusAlwaysLong()
+  }
+
+  @Post('/always-long')
+  async postStatusAlwaysLong(@Req() req): Promise<object> {
+    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.body)
+    return this.powerService.getStatusAlwaysLong()
+  }
+
+  @Get('/getdb')
+  async getAllFromDb(@Req() req): Promise<object> {
+    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip)
+    const data = await this.powerService.getAll()
+    return { count: data.length, data }
+  }
+
+  @Get('/ports')
+  async getLatestPorts(@Req() req): Promise<object> {
+    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip)
+    return this.powerService.getLatestPorts()
+  }
+
+  @Post('/ports')
+  async postLatestPorts(@Req() req): Promise<object> {
+    console.log(moment().utcOffset('+0500'), req.path, req.method, req.ip, req.body)
+
+    const data: Partial<Power> = {
+      ip: req.ip,
+    }
+
+    const outputPorts = req.body.OutputPorts
+    if (outputPorts && typeof outputPorts === 'object') {
+      for (let i = 1; i <= 6; i++) {
+        const value = outputPorts[String(i)]
+        if (typeof value !== 'undefined') {
+          data[`out${i}`] = value
+        }
+      }
+    }
+
+    await this.powerService.saveOutpusts(data)
+    return this.powerService.getLatestPorts()
   }
 }
--- src/modules/power/power.entity.ts
+++ src/modules/power/power.entity.ts
@@ -23,6 +23,24 @@ export class Power {
   @Column({ type: 'int', nullable: false, default: 0 })
   out6: number
 
+  @Column({ type: 'int', nullable: false, default: 0 })
+  in1: number
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  in2: number
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  in3: number
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  in4: number
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  in5: number
+
+  @Column({ type: 'int', nullable: false, default: 0 })
+  in6: number
+
   @Column()
   ip: string
 
